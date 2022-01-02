8856ad16db998d7b5087c6abbde68dd665b36720
Andrii <fedorivandrij@gmail.com>
2022-01-02 11:29:14 +0000

    fix it

--- server.js
+++ server.js
@@ -1,54 +1,33 @@
-// This is simple email sender by programming mentor
+// This is simple sample how to serve static website and save form data to file with NodeJS
+// Author: programming mentor
 // Usage:
-// Install deps: npm i express nodemailer body-parser
-// Run: GMAIL=example@google.com PASSWORD=12345 node server.js
+// 1. Install dependencies: npm i
+// 2. Run server: node server.js
 
 const express = require('express');
-const nodemailer = require('nodemailer');
-
 const bodyParser = require('body-parser');
 const path = require('path');
-
-const GMAIL = process.env.GMAIL;
-const PASSWORD = process.env.PASSWORD;
-
-console.log(GMAIL, PASSWORD);
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: GMAIL,
-        pass: PASSWORD
-    }
-});
-
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, './')));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.post('/order', (req, res) => {
+  const fs = require('fs');
+  fs.appendFile('./orders.txt', JSON.stringify(req.body) + '\n', function(err) {
+    if (err) {
+      res.status(500).send('Server error');
+      return console.log(err);
+    }
+    console.log('Data saved: ' + JSON.stringify(req.body));
+    res.send('Data saved');
+  });
 });
 
-app.post('/email/:address', (req, res) => {
-    const mailOptions = {
-        from: GMAIL,
-        to: req.params.address,
-        subject: 'New email from form-sender',
-        html: JSON.stringify(req.body)
-    };
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.error(error);
-            res.status(500).send('Server error');
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.send(`Adress: ${req.params.address}, Data sent: ${JSON.stringify(req.body)}`);
-        }
-    });
-});
+console.log(
+  'Server is running on',
+  process.env.PORT || 3000,
+  process.env.IP || '0.0.0.0'
+);
 
-console.log('Server is running on', process.env.PORT || 3000, process.env.IP || '0.0.0.0');
-
-app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0');
